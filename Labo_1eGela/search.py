93b81dae954c546608335cd8145e0d2cb12e2f77
Mikel <mikelon.aranburu@gmail.com>
2025-09-30 17:49:32 +0200

    small fix

--- Labo_1eGela/search.py
+++ Labo_1eGela/search.py
@@ -166,7 +166,6 @@ def nullHeuristic(state, problem=None):
 
 def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
-    "*** YOUR CODE HERE ***"
     from util import PriorityQueue
     pq = PriorityQueue()
     visited = set()
